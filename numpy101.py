77fbe106dd3b1774a9d8077c5a3c6d0b41f2ecd1
kelele67 <kingand67@outlook.com>
2018-05-10 19:19:17 +0800

    up

--- numpy101.py
+++ numpy101.py
@@ -186,3 +186,126 @@ sepallength = np.genfromtxt(url, delimiter=',', dtype='float', usecols=[0])
 # Sol
 arr = np.percentile(sepallength, q=[5, 95])
 print(arr)
+
+# Input
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+iris_2d = np.genfromtxt(url, delimiter=',', dtype='object')
+# Method 1
+i, j = np.where(iris_2d)
+np.random.seed(100)
+iris_2d[np.random.choice((i), 20), np.random.choice((j), 20)] = np.nan
+print(iris_2d[:10])
+# Method 2
+np.random.seed(100)
+iris_2d[np.random.randint(150, size=20), np.random.randint(4, size=20)] = np.nan
+print(iris_2d[:10])
+
+# Input
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+iris_2d = np.genfromtxt(url, delimiter=',', dtype='float', usecols=[0,1,2,3])
+iris_2d[np.random.randint(150, size=20), np.random.randint(4, size=20)] = np.nan
+# Sol
+print("Number of missing values: \n", np.isnan(iris_2d[:, 0]).sum())
+print("Position of missing values: \n", np.where(np.isnan(iris_2d[:, 0])))
+
+# Input
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+iris_2d = np.genfromtxt(url, delimiter=',', dtype='float', usecols=[0,1,2,3])
+# Sol
+condition = (iris_2d[:, 2] > 1.5) & (iris_2d[:, 0] < 5.0)
+print(iris_2d[condition])
+
+# Input
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+iris_2d = np.genfromtxt(url, delimiter=',', dtype='float', usecols=[0,1,2,3])
+iris_2d[np.random.randint(150, size=20), np.random.randint(4, size=20)] = np.nan
+# Sol
+# Method 1
+any_nan_in_row = np.array([~np.any(np.isnan(row)) for row in iris_2d])
+print(iris_2d[any_nan_in_row][:5])
+# Method 2
+print(iris_2d[np.sum(np.isnan(iris_2d), axis=1) == 0][:5])
+
+# Input
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+iris = np.genfromtxt(url, delimiter=',', dtype='float', usecols=[0,1,2,3])
+# Method 1
+print(np.corrcoef(iris[:, 0], iris[:, 2])[0, 1])
+# Method 2
+from scipy.stats.stats import pearsonr
+corr, p_value = pearsonr(iris[:, 0], iris[:, 2])
+print(corr)
+
+# Input
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+iris_2d = np.genfromtxt(url, delimiter=',', dtype='float', usecols=[0,1,2,3])
+print(np.isnan(iris_2d).any())
+
+# Input
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+iris_2d = np.genfromtxt(url, delimiter=',', dtype='float', usecols=[0,1,2,3])
+iris_2d[np.random.randint(150, size=20), np.random.randint(4, size=20)] = np.nan
+# Sol
+iris_2d[np.isnan(iris_2d)] = 0
+print(iris_2d[:4])
+
+# Import iris keeping the text column intact
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+iris = np.genfromtxt(url, delimiter=',', dtype='object')
+names = ('sepallength', 'sepalwidth', 'petallength', 'petalwidth', 'species')
+# Sol
+species = np.array([row.tolist()[4] for row in iris])
+print(np.unique(species, return_counts=True))
+
+# Input
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+iris = np.genfromtxt(url, delimiter=',', dtype='object')
+names = ('sepallength', 'sepalwidth', 'petallength', 'petalwidth', 'species')
+# Bin petallength
+petal_length_bin = np.digitize(iris[:, 2].astype('float'), [0, 3, 5, 10])
+# Map it to respective category
+label_map = {1:'small', 2:'medium', 3:'large', 4:np.nan}
+petal_length_cat = [label_map[x] for x in petal_length_bin]
+# View
+print(petal_length_cat[:4])
+
+# Input
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+iris_2d = np.genfromtxt(url, delimiter=',', dtype='object')
+# Sol
+# Compute volume
+sepallength = iris_2d[:, 0].astype('float')
+petallength = iris_2d[:, 2].astype('float')
+volume = (np.pi * petallength * (sepallength**2)) / 3
+# Introduce new dimension to match iris_2d's
+volume = volume[:, np.newaxis]
+# Add the new column
+out = np.hstack([iris_2d, volume])
+# View
+print(out[:4])
+
+# Import iris keeping the text column intact
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+iris = np.genfromtxt(url, delimiter=',', dtype='object')
+# Sol
+# Get the species column
+species = iris[:, 4]
+# Generate Probablistically
+np.random.seed(100)
+a = np.array(['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'])
+secies_out = np.random.choice(a, 150, p=[0.5, 0.25, 0.25])
+# Probablistic Sampling (preferred)
+np.random.seed(100)
+probs = np.r_[np.linspace(0, 0.500, num=50), np.linspace(0.501, .750, num= 50), np.linspace(.751, 1.0, num=50)]
+index = np.searchsorted(probs, np.random.random(150))
+species_out = species[index]
+print(np.unique(species_out, return_counts=True))
+
+# Import iris keeping the text column intact
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+iris = np.genfromtxt(url, delimiter=',', dtype='object')
+# Sol
+# Get the species and petal length columns
+petal_len_setosa = iris[iris[:, 4] == b'Iris-setosa', [2]].astype('float')
+# Get the second last value
+print(np.unique(np.sort(petal_len_setosa))[-2])
